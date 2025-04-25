8dc0818a2053cdd6d9f2a2cf8712f269c208051c
rafaelabarbieric <rafaelabarbieric@gmail.com>
2025-04-25 14:52:49 -0300

    feat: criação api cor

--- app/urls.py
+++ app/urls.py
@@ -8,11 +8,12 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet
+from core.views import CorViewSet, UserViewSet
 
 router = DefaultRouter()
 
 router.register(r'acessorios', AcessorioViewSet, basename='acessorios')
+router.register(r'cores', CorViewSet, basename='cores')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
