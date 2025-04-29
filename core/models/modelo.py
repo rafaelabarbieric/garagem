21bceeb203bb625163e1b8741d258285ef8879fd
rafaelabarbieric <rafaelabarbieric@gmail.com>
2025-04-29 08:32:26 -0300

    feat: criação da api modelo

---
+++ core/models/modelo.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from django.template.defaultfilters import upper
+
+class Modelo(models.Model):
+    nome = models.CharField(max_length=80)
+    marca = models.CharField(max_length=80, null=True, blank=True)
+    categoria = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id} {self.nome.upper()} {self.marca}"
